f8420fc2213f8f8bffa5f3a882b448aa9fa8779e
kdubasov <cergocergo41@gmail.com>
2024-10-07 11:33:38 +0300

    add text to video

--- src/widgets/video/ui/Video.tsx
+++ src/widgets/video/ui/Video.tsx
@@ -6,9 +6,15 @@ export const Video = () => {
   return (
     <div className={styles.container}>
       <BlockHeader
+        style={{ marginBottom: '1.5em' }}
         subtitle="– демонстрация платформы"
         title="Посмотрите, как работает наша платформа"
       />
+      <p>
+        Платформа отлично подходит для работы на компьютере, планшете и
+        мобильных устройствах. Вы можете пользоваться её помощью с любого
+        устройства и в любое время.
+      </p>
 
       <video
         preload="auto"
